1ff169c5b6a213be281c8f03ff307c551bbe0352
SUMAN SAHA <40683051+RAVANREX@users.noreply.github.com>
2025-05-29 08:15:57 +0530

    29/05/2025-8:15
    29/05/2025-8:15

--- My Projects/JustJava/JNITest.java
+++ My Projects/JustJava/JNITest.java
@@ -1,3 +1,29 @@
 public class JNITest {
+    static int x =0;
+    public static void main(String[] args){
+//        Runnable run = new Runnable() {
+//            @Override
+//            public void run() {
+//                x=9;
+//                System.out.println("my Name"+x);
+//            }
+//        };
+
+        Runnable run = () ->{
+                x=9;
+                System.out.println("my Name"+x);
+        };
+
+
+        new Thread(run).;
+    }
+
+//   void show(final int x) {
+//        x = x + 1;  // Error
+//    }
 
 }
+
+// class A {
+//
+//}
